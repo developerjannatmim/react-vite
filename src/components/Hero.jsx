17bf65da688c4b50cf92f2308f2e4f2626b6e96d
developerjannatmim <mim266758@gmail.com>
2023-10-21 18:14:57 +0600

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <img src={"https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="}/>
+        </div>
+    );
+};
+
+export default Hero;
