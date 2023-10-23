d468888a50fd9b69cd3aec88ffe815698e58616b
jannatsoftit <jannatlamitlogiko@gmail.com>
2023-10-24 00:18:00 +0600

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,51 @@
-import React from 'react';
-import Sidebar from './components/Sidebar';
-import Header from './components/Header';
-import Home from './components/Home';
-import User from './components/User';
-import Footer from './components/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Classroom from './components/Classroom';
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import User from "./components/User";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Classroom from "./components/Classroom";
 
 const App = () => {
-
   return (
-    <div className='d-flex'>
-      <div className='w-auto'>
-        <Sidebar/>
-
+    <>
+    <BrowserRouter>
+    <div>
+      <Header/>
+    </div>
+      <div className="d-flex">
+        <div className="w-auto">
+          <Sidebar />
+        </div>
+        <div className="col overflow-auto">
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Home />
+                }
+              ></Route>
+              <Route
+                path="/users"
+                element={
+                  <User />
+                }
+              ></Route>
+              <Route
+                path="/class"
+                element={
+                  <Classroom />
+                }
+              ></Route>
+            </Routes>
+        </div>
       </div>
-      <div className='col overflow-auto'>
-        <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<><Header/><Home/></>}></Route>
-          <Route path='/users' element={<User/>}></Route>
-          <Route path='/class' element={<Classroom/>}></Route>
-          <Route path='/class' element={<Classroom/>}></Route>
-        </Routes>
-        </BrowserRouter>
+      <div>
         <Footer/>
       </div>
-    </div>
+      </BrowserRouter>
+    </>
   );
 };
 
